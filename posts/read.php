ea2b5a4ffe5bcd3e941db67777e346d97cc2d102
Banoulka <nathanbammy@hotmail.co.uk>
2019-10-06 16:24:21 +0100

    added announcement box

--- posts/read.php
+++ posts/read.php
@@ -5,7 +5,7 @@ $view = new stdClass();
 $view->title = "View Post - uGame";
 $view->page = "create-post";
 $view->user = true;
-$view->owner = false;
+$view->owner = true;
 $view->tags = ["Action", "Survival", "Adventure", "Horror"];
 
 require_once("../views/posts/read.phtml");
