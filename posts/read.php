fdddac08da7e9c8ae9d0ddd421b46cd8b9019cc2
Banoulka <nathanbammy@hotmail.co.uk>
2019-10-06 16:05:10 +0100

    changed some content settings

--- posts/read.php
+++ posts/read.php
@@ -1,1 +1,11 @@
 <?php
+
+
+$view = new stdClass();
+$view->title = "View Post - uGame";
+$view->page = "create-post";
+$view->user = true;
+$view->owner = false;
+$view->tags = ["Action", "Survival", "Adventure", "Horror"];
+
+require_once("../views/posts/read.phtml");
